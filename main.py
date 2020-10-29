a9192402b2875cb0398ca739295b2989fd6f4350
Sarthak Vashisth <sarthakvashisth@outlook.com>
2020-10-29 17:38:23 +0530

    main.py file edited

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ app=Flask(__name__)
 
 @app.route("/")
 def hello():
-	return 'Hello World!'
+	str1 = """<h1>Welcome to flask Blog</h1>
+			<p>This is sample</p>
+	"""
+	return str1
 
 
 if __name__ == '__main__':
