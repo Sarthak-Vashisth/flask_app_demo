ce65fdb9fba4438e8f0a38a28dc395987aff08f1
Sarthak Vashisth <sarthakvashisth@outlook.com>
2020-11-04 19:06:41 +0530

    main and requirements original

--- main.py
+++ main.py
@@ -1,52 +1,6 @@
-from flask import Flask, jsonify, request
-from google.cloud import ndb
-from flask_restful import Api, Resource
+from flask import Flask
 
 app=Flask(__name__)
-api = Api(app)
-
-
-class SampleUser(ndb.Model):
-	name = ndb.StringProperty()
-	email = ndb.StringProperty()
-	phone = ndb.StringProperty()
-	sex = ndb.StringProperty()
-	address = ndb.StringProperty()
-
-
-class UserApi(Resource):
-	def post(self):
-		json_data = request.get_json(force=True)
-		user_name = json_data['name']
-		user_email = json_data['email']
-		user_phone = json_data['phone']
-		user_sex = json_data['sex']
-		user_address = json_data['address']
-		client = ndb.Client()
-		with client.context():
-			user_info = SampleUser(name=user_name, email=user_email,phone=user_phone,sex=user_sex,address=user_address)
-			user_info.put()
-
-class GetUserData(Resource):
-	def get(self):
-		user_list=[]
-		query = SampleUser.query()
-		print(query)
-		client = ndb.Client()
-		with client.context():
-			for _ in query:
-				user_temp_obj = {}
-				user_temp_obj['name'] = _.name
-				user_temp_obj['email'] = _.email
-				user_temp_obj['phone'] = _.phone
-				user_temp_obj['sex'] = _.sex
-				user_temp_obj['address'] = _.address
-				user_list.append(user_temp_obj)
-		return jsonify(user_list)
-
-
-api.add_resource(UserApi, '/save_user_data')
-api.add_resource(GetUserData, '/get_user_data')
 
 @app.route("/")
 def hello():
