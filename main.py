25675ecb9d7802117ab4a0e2ac6753e14ec754ca
Sarthak Vashisth <sarthakvashisth@outlook.com>
2020-11-11 20:16:42 +0530

    credential removed

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ firebaseConfig = {
 # firebase = pyrebase.initialize_app()
 
 #cred = credentials.Certificate("C:/Users/sarth/Downloads/flask-app-backend-updated-firebase-adminsdk-h5zoj-d27f731c99.json")
-firebase_app = firebase_admin.initialize_app("/home/sarthak_vashisth15/flask_app_demo/flask-app-backend-updated-firebase-adminsdk-h5zoj-d27f731c99.json")
+firebase_app = firebase_admin.initialize_app()
 auth = firebase_admin.auth.Client(firebase_app)
 # firebase_auth=firebase.auth()
 
